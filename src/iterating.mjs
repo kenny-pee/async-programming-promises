6da261ef3a75434cde498b4648c761e7c00d2a00
Kenny Pee <70102666+kenny-pee@users.noreply.github.com>
2022-02-27 20:50:35 +0800

    Async Await Practice

--- src/iterating.mjs
+++ src/iterating.mjs
@@ -1,9 +1,17 @@
 import setText , {appendText} from './results.mjs';
 
-export function get(){
+export async function get(){
+    const {data} = await axios.get('http://localhost:3000/orders/1')
+    setText(JSON.stringify(data))
 }
 
-export function getCatch(){
+export async function getCatch(){
+    try{
+        const { data } = await axios.get('http://localhost:3000/orders/123')
+        setText(JSON.stringify(data));
+    } catch (error) {
+        setText(error);
+    } 
 }
 
 export function chain(){
